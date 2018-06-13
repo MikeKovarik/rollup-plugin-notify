d486d6cbdd06de85f99ed11fc238fddc1f463104
Mike <kenr.mk@gmail.com>
2018-06-13 11:31:39 +0200

    cleanup

--- example/transpiled.js
+++
@@ -1,9 +0,0 @@
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('anchora')) :
-	typeof define === 'function' && define.amd ? define(['anchora'], factory) :
-	(factory(global.anchora));
-}(this, (function (anchora) { 'use strict';
-
-	console.log(anchora.createServer());
-
-})));
